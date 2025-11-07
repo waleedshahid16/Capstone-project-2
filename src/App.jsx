45fab7f78e4c85f405ed8b9923ac1541feaaa103
Waleed Shahid <waleedshahid838@gmail.com>
2025-11-08 01:48:11 +0500

    Full e-commerce app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import ProductDetailPage from "./components/ProductDetailPage";
+import AboutUs from "./components/AboutUs";
+import ShippingPolicy from "./components/ShippingPolicy";
+import ReturnPolicy from "./components/ReturnPolicy";
+import Checkout from "./components/Checkout";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+import ContactUs from "./components/auth/ContactUs";
 
 function App() {
   return (
@@ -15,10 +20,15 @@ function App() {
           <Route index element={<Home />} />
           <Route path="products" element={<Products />} />
           <Route path="product/:id" element={<ProductDetailPage />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="shipping-policy" element={<ShippingPolicy />} />
+          <Route path="return-policy" element={<ReturnPolicy />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   );
